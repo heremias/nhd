532ba96b4c38f946a920b2b6747e33399cc78731
Jeremy Evans <jeremyevans@Jeremys-MacBook-Pro.local>
2022-07-15 15:13:48 -0700

    0.1.5.

---
+++ dnd/build.sh
@@ -0,0 +1,32 @@
+composer config -g allow-plugins.composer/installers true
+composer config -g allow-plugins.drupal/core-composer-scaffold true
+composer config -g allow-plugins.drupal/core-project-message true
+composer config -g allow-plugins.oomphinc/composer-installers-extender true
+composer create-project drupal/recommended-project:9.4.2 drupal
+ln -s /root/.composer/vendor/bin/drush /usr/local/bin/drush
+
+composer global require drush/drush
+cd drupal
+rm -rf vendor/bin/drush
+composer global update
+
+composer install --no-interaction
+composer require drush/drush
+chmod 777 web/sites/default
+
+cd web
+
+drush site-install -y --db-url=sqlite://../drupal.sqlite
+mv /var/www/.htaccess /var/www/drupal/web
+chmod -R g+rw /var/www/drupal
+chown -R :www-data /var/www/drupal
+chmod -R 775 /var/www/drupal/drupal.sqlite
+mv /var/www/mods.sh /var/www/drupal/mods.sh
+mv /var/www/setup.sh /var/www/drupal/setup.sh
+mv /var/www/getuuid.sh /var/www/drupal/getuuid.sh
+
+cd /var/www/drupal/web/sites/default
+chmod 777 -R files
+cd /var/www/drupal
+drush uli
+
