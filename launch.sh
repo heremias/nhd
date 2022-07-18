2ce820711d85b9af6c3f4beedd3731ed1311d21b
Jeremy Evans <jeremyevans@Jeremys-MacBook-Pro.local>
2022-07-18 10:01:33 -0700

    PHP 8

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 git log -1 --pretty=%h > commit.txt
 tag=$(<commit.txt)
-docker build -t ylocal:$tag .
-docker tag ylocal:$tag ylocal:latest
-docker run -d --name BASE -p 8080:80 ylocal:latest
-docker ps | grep 'BASE' | awk '{print $1}' > cid.txt
+docker build -t ybase:$tag .
+docker tag ybase:$tag ybase:latest
+docker run -d --name TEST -p 8080:80 ybase:latest
+docker ps | grep 'TEST' | awk '{print $1}' > cid.txt
 cid=$(<cid.txt)
